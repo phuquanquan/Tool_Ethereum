a7d434c680741859896cad6fcc91db93867c9a98
Tran Phu Quan <tranphuquan0101@gmail.com>
2023-03-30 08:58:37 +0700

    Update

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ class Ethereum_Balance:
 
 
 if __name__ == "__main__":
-    # module = "account"
-    # action = "balancemulti"
-    # address = "0xd0e45Ccf9B245fc848c910D49B8DE5834859980f,0xEF04aF269911C3EF2aadd77e2F610343Da0B49a4,0x0A1ecE6959B30DA3a46D86f02488FBf190f07dd0,"
-    # tag = "latest"
-    # apikey = "NSN54XGSU8M8I6C91FY4QY47GQ9D3V3RGM"
+    module = "account"
+    action = "balancemulti"
+    address = "0xd0e45Ccf9B245fc848c910D49B8DE5834859"
+    tag = "latest"
+    apikey = "NSN54XGSU8M8I6C91FY4QY47GQ9D3V3RGM"
+
+    Ethereum_Balance(module, action, address, tag, apikey).get_balance()
+
+    # with open('./data/Demo_Etherscan.txt', 'r') as file:
     #
-    # Ethereum_Balance(module, action, address, tag, apikey).get_balance()
-
-    with open('./data/Demo_Etherscan.txt', 'r') as file:
-
-        # reading each line
-        for line in file:
-
-            # reading each word
-            for word in line.split():
-                # displaying the words
-                print(word)
+    #     # reading each line
+    #     for line in file:
+    #
+    #         # reading each word
+    #         for word in line.split():
+    #             # displaying the words
+    #             print(word)
